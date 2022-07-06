946660d8587ce9b3676d42c26eeeb5915034149f
Shah Nirved <95268774+Nirves@users.noreply.github.com>
2022-07-06 10:22:13 +0530

    Add files via upload

---
+++ login and registration system project.cpp
@@ -0,0 +1,92 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <fstream> 
+#include <cstring>
+using namespace std;
+bool IsloggedIn()
+{
+    string username, password, un, pw; 
+
+    cout << "Enter username :";
+    cin >> username;
+
+    cout << "Enter password :";
+    cin >> password;
+
+    
+    ifstream read("data.txt"); 
+    getline(read, un);
+    getline(read, pw); 
+
+    
+
+    if (un == username && pw == password)
+    {
+        return true;   
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+   
+    int choice;
+
+    cout << "1 : Register \n2 :Login\nYour choice :";
+    cin >> choice;
+    if (choice == 1)
+    {
+        string username, password;
+
+        cout << "select a username :";
+        cin >> username;
+        cout << "select a password :";
+        cin >> password;
+
+        
+
+        ofstream file;
+        
+        file.open("data.txt");
+
+  
+        file<<username<<endl;
+        file<<password<<endl; //the data is registered into our file
+        file.close();
+
+    
+    main();
+    }
+
+
+    else if(choice==2)
+    {
+        
+       bool status =IsloggedIn();
+
+       if(!status) 
+       {
+          cout<<"Invalid user name password :"<<endl;
+          system("PAUSE");
+
+
+          return 0;
+       }
+       else 
+       {
+           cout<<"Successfully Logged in"<<endl;
+           system("pause");
+           return 1;
+       }
+    }
+}
